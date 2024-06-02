6a2f8dac1d2d99493634f04a9769ae8fdabaff45
MorS <derictor228@gmail.com>
2024-06-02 17:50:05 +0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -13,15 +13,17 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	ln, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+	conn, err := ln.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	message := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	conn.Write(message)
 }
