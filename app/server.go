4556ab79698fc92914201d7f491171311002f0b1
MorS <derictor228@gmail.com>
2024-06-03 14:42:17 +0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"net"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 	
 	ln, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -20,24 +18,45 @@ func main() {
 		os.Exit(1)
 	}
 	
-	conn, err := ln.Accept()
-
 	defer ln.Close()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 	
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go HandleConnection(conn)
+	}
+}
+
+func HandleConnection(conn net.Conn) {
+	defer conn.Close()
+
 	req, err := http.ReadRequest(bufio.NewReader(conn))
 	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading request", err.Error())
+		return
 	}
 
-	if req.URL.Path == "/" {
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if strings.HasPrefix(req.URL.Path, "/echo/") {
+		echoStr := strings.TrimPrefix(req.URL.Path, "/echo/")
+		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoStr), echoStr)
+		_, err := conn.Write([]byte(response))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+		}
+	} else if req.URL.Path == "/" {
+		_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+		}
+
 	} else {
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		_, err := conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+		}
 	}
-
 }
