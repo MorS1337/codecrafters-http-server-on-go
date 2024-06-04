446874e648d143c2ce51a97d8e864fdf28e8761b
MorS <derictor228@gmail.com>
2024-06-04 12:36:35 +0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -45,7 +45,7 @@ func HandleConnection(conn net.Conn) {
 	if strings.HasPrefix(req.URL.Path, "/echo/") {
 		echoStr := strings.TrimPrefix(req.URL.Path, "/echo/")
 		contentEncStr := CheckEncoding(req)
-		fmt.Print(contentEncStr)
+		fmt.Print(len(req.Header))
 		if contentEncStr != "invalid-encoding" && contentEncStr != "" {
 			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\nContent-Encoding: %s\r\n\r\n%s", len(echoStr), contentEncStr, echoStr)
 		} else {
